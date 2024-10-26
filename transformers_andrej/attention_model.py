ed7b87c0f555ad44bd699f992d362dab0e56cb6b
mgteus <mateus.guimaraes048@gmail.com>
2024-10-26 11:58:04 -0300

    adicionando multi head attention.

--- transformers_andrej/attention_model.py
+++ transformers_andrej/attention_model.py
@@ -9,12 +9,11 @@ def load_data(split):
     df = pd.read_parquet(r'/home/mgteus/workspace/neuro/transformers_andrej/train_run.gzip')
     feature_array = []
     for x_pos,y_pos in zip(df['x_pos'], df['y_pos']):
-            feature_array.append(torch.tensor([x_pos, y_pos]))
-
+            feature_array.append(np.array([x_pos, y_pos], dtype='double'))
     feature_array = np.array(feature_array)
-    feature_array = torch.tensor(feature_array)
+    feature_array = torch.from_numpy(feature_array)
+    feature_array = feature_array.float()
     n = int(len(df)*0.8)
-    
     return feature_array[:n] if split == 'train' else feature_array[n:]
 
 def get_batch1d(context_len, batch_size, split, device):
@@ -31,6 +30,7 @@ def get_batch2d(context_len, batch_size, split, device):
     ix = torch.randint(len(data) - context_len -1, (batch_size,))
     x = torch.stack([data[i:i+context_len] for i in ix])
     y = torch.stack([data[i+1:i+context_len+1] for i in ix])
+
     x, y = x.to(device), y.to(device)
     return x, y
 
@@ -85,34 +85,87 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, num_heads, context_len, batch_size, dropout):
+        super().__init__()
+        self.context_len = context_len
+        self.batch_size = batch_size
+        self.dropout = dropout
+        self.num_heads = num_heads
+        
+        self.linear_x = nn.Linear(3, 1, bias=False)
+        self.linear_y = nn.Linear(3, 1, bias=False)
+        self.linear_both = nn.Linear(6, 2, bias=False)
+
+        # creating the multi heads
+        self.heads = nn.ModuleList(
+            [ Head(context_len=self.context_len
+                    , batch_size=self.batch_size
+                    , dropout=self.dropout)
+                for _ in range(num_heads)
+                    ]
+                    )
+    def forward(self, x):
+        concat_table = torch.cat(
+                [h(x) for h in self.heads]
+                , dim=-1
+                )
+        output = self.linear_both(concat_table)
+        # output = torch.empty(concat_table.shape[0], concat_table.shape[1], 2)
+
+        # # Aplicar as transformações na última dimensão
+        # for i in range(concat_table.shape[0]):  # Iterar sobre a primeira dimensão
+        #     for j in range(concat_table.shape[1]):  # Iterar sobre a segunda dimensão
+        #         even_input = concat_table[i, j, 0:3]  # Pega 3 elementos para índices pares
+        #         odd_input = concat_table[i, j, 3:6]   # Pega 3 elementos para índices ímpares
+                
+        #         output[i, j, 0] = self.linear_x(even_input).item()  # Resultado para pares
+        #         output[i, j, 1] = self.linera_y(odd_input).item()    # Resultado para ímpares
+
+        return output
+
+
 
 if __name__ == '__main__':
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     CONTEXT_LEN = 30
     BATCH_SIZE = 16
     DROPOUT = 0.1
-    LEARNING_RATE = 1e-3
-    NUM_EPOCHS = 1
+    LEARNING_RATE = 1e-4
+    NUM_EPOCHS = 1000
+    NUM_HEADS = 3
 
-    model = Head(context_len=CONTEXT_LEN, batch_size=BATCH_SIZE, dropout=DROPOUT)
+
+    # model = Head(context_len=CONTEXT_LEN, batch_size=BATCH_SIZE, dropout=DROPOUT)
+    model = MultiHeadAttention(
+                  num_heads = NUM_HEADS
+                , context_len = CONTEXT_LEN
+                , batch_size = BATCH_SIZE
+                , dropout = DROPOUT)
+    model = model.to(DEVICE)
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     
 
 
+ 
     loss_list = []
 
     for epoch in range(NUM_EPOCHS):
         xb, yb = get_batch2d(context_len=CONTEXT_LEN, batch_size=BATCH_SIZE, split='train', device=DEVICE)
         optimizer.zero_grad(set_to_none=True)
         predictions = model(xb)
+        predictions = predictions.to(DEVICE)
+        print(predictions.shape)
         loss = RMSELoss(predictions, yb)
-        loss_list.append(loss)
+        print(loss)
         loss.backward()
         optimizer.step()
+        loss_list.append(loss.cpu().detach().numpy())
         print(f"iter. {epoch} - loss = {loss.item():4f}")
 
-    # plt.plot(loss_list)
-    # plt.show()
+    plt.plot(loss_list)
+    plt.show()
 
 
 
