65f11d91602f69d1730ed59826a5282d8627a8c7
mgteus <mateus.guimaraes048@gmail.com>
2024-11-30 11:45:58 -0300

    adicionando arquivo com 100 runs.

--- transformers_andrej/attention_utils.py
+++ transformers_andrej/attention_utils.py
@@ -13,7 +13,7 @@ def concat_runs(paths:list = [], final_path:str = ''):
     file_count = 0
     files_to_concat = []
     for file in glob(r'runs/15min/**.gzip'):
-        if file_count < 30:
+        if file_count < 100:
             files_to_concat.append(file)
             file_count+=1
     
@@ -21,7 +21,7 @@ def concat_runs(paths:list = [], final_path:str = ''):
     dfs_list = [pd.read_parquet(file) for file in files_to_concat]
     df = pd.concat(dfs_list, ignore_index=True)
     print(df.shape)
-    df.to_parquet(r'transformers_andrej/train_runs_15.gzip')
+    df.to_parquet(r'transformers_andrej/train_runs_15_100.gzip')
     return
 
 
